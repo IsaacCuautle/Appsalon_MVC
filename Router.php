e86d5906eed4986f74c6218972ce705d4fca22c6
Luis Isaac Cuautle <cuautle.tron@gmail.com>
2022-05-19 01:29:54 -0500

    intento

--- Router.php
+++ Router.php
@@ -8,12 +8,12 @@ class Router
     public array $getRoutes = [];
     public array $postRoutes = [];
 
-    public function get($url, callable $fn)
+    public function get($url,  $fn)
     {
         $this->getRoutes[$url] = $fn;
     }
 
-    public function post($url, callable $fn)
+    public function post($url,  $fn)
     {
         $this->postRoutes[$url] = $fn;
     }
