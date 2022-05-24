2d281a0a7fba720c618b54f3611fd90a53f906da
Luis Isaac Cuautle <cuautle.tron@gmail.com>
2022-05-24 17:57:07 -0500

    Modificacion en Router.php

--- Router.php
+++ Router.php
@@ -21,22 +21,34 @@ class Router
     public function comprobarRutas()
     {
         session_start();
-        if (isset($_SERVER['PATH_INFO'])) {
-            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        } else {
-            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        }
-        
+        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
+        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
+        $splitURL = explode('?', $currentUrl);
+        // debuguear($splitURL);
+
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$currentUrl] ?? null;
+            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
         } else {
-            $fn = $this->postRoutes[$currentUrl] ?? null;
+            $fn = $this->postRoutes[$splitURL[0]] ?? null;
         }
+        // if (isset($_SERVER['PATH_INFO'])) {
+        //     $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        // } else {
+        //     $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        // }
+
+        // $method = $_SERVER['REQUEST_METHOD'];
+
+        // if ($method === 'GET') {
+        //     $fn = $this->getRoutes[$currentUrl] ?? null;
+        // } else {
+        //     $fn = $this->postRoutes[$currentUrl] ?? null;
+        // }
 
 
-        if ( $fn ) {
+        if ($fn) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
         } else {
